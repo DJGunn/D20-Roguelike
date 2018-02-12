deb17b72089b8e326ea08b8a99aa88979d46bf8c
gwyngerlits <gwyngerlits@gmail.com>
2018-02-11 21:25:41 -0800

    Made New Game do something

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+    public void LoadNewScene(string scene) {
+        //Application.LoadLevel(scene);
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+}
